35fe5477a6e7d357471f2915786f45ca2fca3222
Cielo Fuentes Oviedo <76010662@ucsm.edu.pe>
2021-08-31 23:14:12 -0500

    Finalización de la Experiencia N°3

---
+++ Script/Site.Master.designer.vb
@@ -0,0 +1,134 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+Namespace script
+
+    Partial Public Class SiteMaster
+
+        '''<summary>
+        '''Control Form2.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents Form2 As Global.System.Web.UI.HtmlControls.HtmlForm
+
+        '''<summary>
+        '''Control Button3.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents Button3 As Global.System.Web.UI.WebControls.Button
+
+        '''<summary>
+        '''Control Label1.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
+
+        '''<summary>
+        '''Control cuerpo.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents cuerpo As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+
+        '''<summary>
+        '''Control Form1.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents Form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+
+        '''<summary>
+        '''Control Select1.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents Select1 As Global.System.Web.UI.HtmlControls.HtmlSelect
+
+        '''<summary>
+        '''Control Button2.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents Button2 As Global.System.Web.UI.HtmlControls.HtmlInputButton
+
+        '''<summary>
+        '''Control formulario.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents formulario As Global.System.Web.UI.HtmlControls.HtmlForm
+
+        '''<summary>
+        '''Control enlace.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents enlace As Global.System.Web.UI.HtmlControls.HtmlAnchor
+
+        '''<summary>
+        '''Control Button1.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents Button1 As Global.System.Web.UI.HtmlControls.HtmlButton
+
+        '''<summary>
+        '''Control WebForm.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents WebForm As Global.System.Web.UI.HtmlControls.HtmlForm
+
+        '''<summary>
+        '''Control lista.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents lista As Global.System.Web.UI.HtmlControls.HtmlSelect
+
+        '''<summary>
+        '''Control etiqueta.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents etiqueta As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+    End Class
+End Namespace
